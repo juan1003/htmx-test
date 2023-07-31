3f09b6cce362134d7771b3130da0de8babb7b1e0
Juan De Jesus <jdejesus93@gmail.com>
2023-07-31 15:30:11 -0500

    Small changes of styling

--- index.js
+++ index.js
@@ -36,10 +36,10 @@ app.get("/contact", function(req, res, next) {
 
 app.get("/clicked", function(req, res, next){
     const template = `
-        <div class="p-3 rounded bg-[#454545] mt-5 ml-3 w-[350px]">
+        <div class="p-3 rounded my-4 mx-3 lg:w-[350px] bg-[#F0F0FF]">
             <h1>Clicked</h1>
             <span id="counter">${counter} times</span>
-            <button class="px-3 bg-red-700" hx-post="/clicked" hx-swap="innerText" hx-target="#counter">Increase</button>
+            <button class="px-3 bg-[#1b77b7] text-white" hx-post="/clicked" hx-swap="innerText" hx-target="#counter">Increase</button>
         </div>
     `
 
